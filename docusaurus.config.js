cbc4b145dcdd248bac20dda4e684b390400e90d8
Volkan TAŞ <djangoogreniyorum@gmail.com>
2023-08-20 05:51:43 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ibrahimdelice.github.io', // Usually your GitHub org/user name.
-  projectName: 'ibrahimdelice', // Usually your repo name.
+  organizationName: 'ibrahimdelice', // Usually your GitHub org/user name.
+  projectName: 'ibrahimdelice.github.io', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
