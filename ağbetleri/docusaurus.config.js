d00d84abb0f0c246a373fcbd73027677eac236da
Deveb <djangoogreniyorum@gmail.com>
2023-08-26 18:46:29 +0300

    Prof. Dr. ve Şair

--- ağbetleri/docusaurus.config.js
+++ ağbetleri/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Hacı İbrahim DELİCE',
-  tagline: 'Prof. Dr.',
+  tagline: 'Prof. Dr. ve Şair',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
