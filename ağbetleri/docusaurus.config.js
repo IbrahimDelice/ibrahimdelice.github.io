903010183143f7fd4b2586a9b7697f1c8addc8c4
Deveb <djangoogreniyorum@gmail.com>
2023-08-27 19:48:05 +0300

    Sosyal paylaşımlar için görsel.

--- ağbetleri/docusaurus.config.js
+++ ağbetleri/docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/ibrahim_DELİCE.png',
       navbar: {
         title: 'İbrahim DELİCE',
         logo: {
